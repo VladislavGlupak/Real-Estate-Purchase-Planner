6426e3cbaf65f1e233b89e6484972b5cb19d8153
Vladislav Glupak <vglupak@gmail.com>
2022-02-15 12:56:52 -0800

    remove matplot

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 # web page name
 st.set_page_config(page_title="Real Estate Purchase Planner in California", page_icon=":eyeglasses:")
