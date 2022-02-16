7e8d929d9c91cd528bfd86cf15efbbce95ca92c7
Vladislav Glupak <vglupak@gmail.com>
2022-02-16 10:03:37 -0800

    changed form and fields

--- app.py
+++ app.py
@@ -22,27 +22,17 @@ st.markdown('---')
 st.sidebar.markdown("# Portfolio")
 
 # define the inputs
-cont_monthly = st.sidebar.slider('Monthly Contribution to Investments?', 0, 10000, 5000) # min, max, default
-pf_type_cont = st.sidebar.radio('Portfolio Type for Contributions?', ['Low risk', 'Medium risk', 'High risk'])
-curr_holdings = st.sidebar.slider('Current Holdings of Portfolio?', 0, 10000, 5000) # min, max, default
-pf_type_hold = st.sidebar.radio('Portfolio Type for Current Holdings?', ['Low risk', 'Medium risk', 'High risk'])
+savings = int(st.sidebar.text_input('Savings, $', '1000'))
+cont_monthly = st.sidebar.slider('Monthly Contribution to Investments, $', 0, 10000, 5000, step=100) # min, max, default
+pf_risk_type = st.sidebar.radio('Portfolio Type?', ['Low risk', 'Medium risk', 'High risk'])
+curr_crypto = cont_monthly = int(st.sidebar.text_input('Number of Ethereum in your portfolio', '10'))
+curr_stocks = cont_monthly = int(st.sidebar.text_input('Number of S&P500 in your portfolio', '10'))
 
 # desired house
 st.sidebar.markdown("# Desired house")
 
-total_price = st.sidebar.slider('Insert a desired prise of house', 0, 10000000, 5000000)
-down_payment = st.sidebar.slider('Down Payment %', 0, 100, 50)
-num_years = st.sidebar.slider('How many yesrs?', 0, 50, 25) # min, max, default
-
-# summarize inputs
-st.markdown('### Your entered data:')
-st.write('Monthly Contribution to Investments, $ - ', cont_monthly)
-st.write('Portfolio Type for Contributions - ', pf_type_cont)
-st.write('Current Holdings of Portfolio, $ - ', curr_holdings)
-st.write('Portfolio Type for Current Holdings - ', pf_type_hold)
-st.write('You entered the desired price $: - ', total_price)
-st.write('Down Payment % - ', down_payment)
-st.write('Number of years - ', num_years)
+total_price = int(st.sidebar.text_input('Desired house price $', '1000000'))
+num_years = st.sidebar.slider('How many yesrs?', 0, 50, 25, step=1) # min, max, default
 
 # Load .env environment variables
 load_dotenv()
