6b6170ba23c8905d6b0878d4097a64ea9dd720e6
Vladislav Glupak <vglupak@gmail.com>
2022-02-18 20:09:10 -0800

    adding map

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ else:
                     df_filtred_by_price = df[ (total_price-(total_price*0.2) <= df['price']) & (df['price'] <= total_price+(total_price*0.2))]
                     df_filtred_by_price = df_filtred_by_price.reset_index(drop=True)
                     
-                    st.markdown(f"### Approximate location of the houses in {desired_city}")
+                    st.markdown(f"### Approximate locations of the houses in {desired_city}")
                     st.map(df_filtred_by_price)
                     st.markdown("### Houses we could find for you based on today's data:")
                     st.markdown("20% range based on desired price.")
